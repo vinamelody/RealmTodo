8cabf40e71e08e208e7fa5bca7066b56e4547831
Vina Melody <vina_melody@yahoo.com>
2017-02-04 17:21:28 +0800

    Link ui elements to view controller

--- RealmTodo/viewcontrollers/CreateTaskTableViewController.swift
+++ RealmTodo/viewcontrollers/CreateTaskTableViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 
 class CreateTaskTableViewController: UITableViewController {
     
+    @IBOutlet weak var taskTitle: UITextField!
+    @IBOutlet weak var taskPriority: UISegmentedControl!
+    @IBOutlet weak var taskUsers: UISegmentedControl!
     
+    @IBAction func createTask(_ sender: Any) {
+        navigationController!.popViewController(animated: true)
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        taskTitle.becomeFirstResponder()
+    }
 }
