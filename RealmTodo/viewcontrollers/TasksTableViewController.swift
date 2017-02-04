945a942c0004ec4fa6dcaee6b28e2ab198324745
Vina Melody <vina_melody@yahoo.com>
2017-02-04 22:09:17 +0800

    Initial TasksTableViewController

--- RealmTodo/viewcontrollers/TasksTableViewController.swift
+++ RealmTodo/viewcontrollers/TasksTableViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class TasksTableViewController: UITableViewController {
     
+    @IBOutlet weak var filterToggle: UIBarButtonItem!
+    
+    var tasks = [String]()
     
     
+    @IBAction func filterToggleTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell") as! TaskCell
+        return cell
+    }
     
 }
