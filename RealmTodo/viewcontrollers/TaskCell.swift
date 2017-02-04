95501fc3bbbf57755d202f41da4235aa094d62ad
Vina Melody <vina_melody@yahoo.com>
2017-02-04 22:08:29 +0800

    Initial TaskCell

---
+++ RealmTodo/viewcontrollers/TaskCell.swift
@@ -0,0 +1,25 @@
+//
+//  TaskCell.swift
+//  RealmTodo
+//
+//  Created by Vina Melody on 4/2/17.
+//  Copyright © 2017 Vina Melody. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+    @IBOutlet weak var checkButton: UIButton!
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var priority: UILabel!
+    
+    
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func awakeFromNib() {
+        
+    }
+}
