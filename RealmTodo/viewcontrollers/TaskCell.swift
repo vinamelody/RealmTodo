48352f4eefe1e4a92ddec80c6338ba64e8c285de
Vina Melody <vina_melody@yahoo.com>
2017-02-05 22:26:29 +0800

    Implement updateTask and configureWithTask

--- RealmTodo/viewcontrollers/TaskCell.swift
+++ RealmTodo/viewcontrollers/TaskCell.swift
@@ -16,10 +16,32 @@ class TaskCell: UITableViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var priority: UILabel!
     
+    var taskId: String?
     
-    @IBAction func checkButtonTapped(_ sender: UIButton) {
+    fileprivate func updateTask(checked: Bool) {
+        if let realm = try? Realm(), let id = self.taskId, let task = realm.object(ofType: Task.self, forPrimaryKey: id) {
+            
+            try! realm.write {
+                task.done = checked
+            }
+            
+            checkButton.isSelected = task.done
+        }
     }
     
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+        print("checked to \(!checkButton.isSelected)")
+        updateTask(checked: !checkButton.isSelected)
+    }
+    
+    func configureWithTask(_ task: Task) {
+        taskId = task.taskId
+        checkButton.isSelected = task.done
+        title.text = task.title
+        priority.text = task.priorityText
+        priority.textColor = task.priorityColor
+        
+    }
     override func awakeFromNib() {
         
     }
